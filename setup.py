9c6652d594b5afeba8279b89268c701457ea0b6f
Angelo Velloso <vellosov@hotmail.com>
2022-04-16 17:14:06 -0400

    Sobre distribuicao - pacote

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     page_description = f.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+#with open("requirements.txt") as f:
+#    requirements = f.read().splitlines()
 
 setup(
     name="get_table_sped",
@@ -14,8 +14,8 @@ setup(
     description="Package to download SPED tables",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/AngeloVelloso/dio-desafio-pypi"
+    url="https://github.com/AngeloVelloso/dio-desafio-pypi",
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=['mechanize', 'pandas'],
     python_requires='>=3.8',
 )
